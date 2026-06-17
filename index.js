db90d77732955d5a0daa6b4793e3c4d904e97ebc
JacksonWeudes <weudes2024jackson@gmail.com>
2026-06-17 15:41:47 -0300

    style: discography layout

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const header = document.querySelector(".header");
+
 function toggleMenu()
 {
     const menuIcon = document.querySelector(".menu-icon");
@@ -9,9 +11,26 @@ function toggleMenu()
         menuIcon.classList.toggle("active");
         overlay.classList.toggle("active");
         document.body.classList.toggle("active");
+
+        if(window.scrollY == 0){
+            header.classList.toggle("active");
+        }
+    });
+}
+
+function activedHeader()
+{
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 10){
+            header.classList.add("active");
+        }
+        else{
+            header.classList.remove("active");
+        }
     })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleMenu();
+    activedHeader();
 })
